71584de74368b265f5f43e8d42585cc4ce25ff7b
Theophile-Kango <fadhilikango@gmail.com>
2024-06-19 18:27:43 +0200

    feat: add CTA buttons

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -2,16 +2,19 @@
 
 import Link from "next/link";
 import type { NextPage } from "next";
-import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { CheckIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { Address } from "~~/components/scaffold-stark";
 import { useAccount } from "@starknet-react/core";
 import { Address as AddressType } from "@starknet-react/chains";
 
+
+
 const Home: NextPage = () => {
   const connectedAddress = useAccount();
+
   return (
     <>
-      <div className="flex items-center flex-col flex-grow pt-10">
+      <main className="flex items-center flex-col flex-grow pt-10">
         <div className="px-5">
           <h1 className="text-center">
             <span className="block text-2xl mb-2">Welcome to</span>
@@ -25,29 +28,120 @@ const Home: NextPage = () => {
         </div>
 
         <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
-          <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <BugAntIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Tinker with your smart contract using the{" "}
-                <Link href="/debug" passHref className="link">
-                  Debug Contracts
-                </Link>{" "}
-                tab.
-              </p>
+          <div className="flex justify-center items-center gap-12 lg:gap-32 flex-col sm:flex-row">
+            <div className="flex flex-col bg-base-100 px-4 lg:px-8 py-5 max-w-lg rounded-xl drop-shadow-2xl">
+              <h5 className="text-3xl font-bold mb-6">For debtor</h5>
+              <ul className="list lh-lg mb-6 ml-4 leading-relaxed lg:leading-[3em]">
+                <li className="flex items-center py-2 md:py-0">
+                  <CheckIcon className="size-6 text-green-300 mr-2"/>
+                  <span>Creating an Ethereum wallet.</span>
+                </li>
+                <li className="flex items-center py-2 md:py-0">
+                  <CheckIcon className="size-6 text-green-300 mr-2"/>
+                  <span>Set up your debtor account</span>
+                </li>
+                <li className="flex items-center py-2 md:py-0">
+                  <CheckIcon className="size-6 text-green-300 mr-2"/>
+                  <span>Deposit funds</span>
+                </li>
+                <li className="flex items-center py-2 md:py-0 leading-normal">
+                  <CheckIcon className="size-6 text-green-300 mr-2"/>
+                  <span>Participate in one-year investment cycles.</span>
+                </li>
+                <li className="flex items-center py-2 md:py-0">
+                  <CheckIcon className="size-6 text-green-300 mr-2"/>
+                  <span>Submit documents for validation</span>
+                </li>
+                <li className="flex items-center py-2 md:py-0">
+                  <CheckIcon className="size-6 text-green-300 mr-2"/>
+                  <span>Complete KYC validation.</span>
+                </li>
+                <li className="flex items-center py-2 md:py-0">
+                  <CheckIcon className="size-6 text-green-300 mr-2"/>
+                  <span>Join investment campaigns</span>
+                </li>
+                <li className="flex items-center py-2 md:py-0">
+                  <CheckIcon className="size-6 text-green-300 mr-2"/>
+                  <span>Track total investments per cycle</span>
+                </li>
+                <li className="flex items-center py-2 md:py-0">
+                  <CheckIcon className="size-6 text-green-300 mr-2"/>
+                  <span>Ensure deposits meet required amounts.
+                  </span>
+                </li>
+                <li className="flex items-center py-2 md:py-0">
+                  <CheckIcon className="size-6 text-green-300 mr-2"/>
+                  <span>Withdraw funds after campaign completion</span>
+                </li>
+                <li className="flex items-center py-2 md:py-0">
+                  <CheckIcon className="size-6 text-green-300 mr-2"/>
+                  <span>Receive returns plus interest.</span>
+                </li>
+              </ul>
+              <Link 
+                href='' 
+                className="bg-emerald-500 shadow-md py-2 px-4 rounded-full text-center text-white font-bold text-xl hover:bg-blue-200 hover:text-cyan-950">
+                  Submit a Project
+              </Link>
             </div>
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Explore your local transactions with the{" "}
-                <Link href="/blockexplorer" passHref className="link">
-                  Block Explorer
-                </Link>{" "}
-                tab.
-              </p>
+            <div className="flex flex-col bg-base-100 px-4 lg:px-8 py-5 max-w-lg rounded-xl drop-shadow-2xl">
+              <h5 className="text-3xl font-bold mb-6">For Creditor</h5>
+              <ul className="list lh-lg mb-6 ml-4 leading-relaxed lg:leading-[3em]">
+                <li className="flex items-center py-2 md:py-0">
+                  <CheckIcon className="size-6 text-green-300 mr-2"/>
+                  <span>Creating an Ethereum wallet.</span>
+                </li>
+                <li className="flex items-center py-2 md:py-0">
+                  <CheckIcon className="size-6 text-green-300 mr-2"/>
+                  <span>Set up your debtor account</span>
+                </li>
+                <li className="flex items-center py-2 md:py-0">
+                  <CheckIcon className="size-6 text-green-300 mr-2"/>
+                  <span>Deposit funds</span>
+                </li>
+                <li className="flex items-center py-2 md:py-0 leading-normal">
+                  <CheckIcon className="size-6 text-green-300 mr-2"/>
+                  <span>Participate in one-year investment cycles.</span>
+                </li>
+                <li className="flex items-center py-2 md:py-0">
+                  <CheckIcon className="size-6 text-green-300 mr-2"/>
+                  <span>Submit documents for validation</span>
+                </li>
+                <li className="flex items-center py-2 md:py-0">
+                  <CheckIcon className="size-6 text-green-300 mr-2"/>
+                  <span>Complete KYC validation.</span>
+                </li>
+                <li className="flex items-center py-2 md:py-0">
+                  <CheckIcon className="size-6 text-green-300 mr-2"/>
+                  <span>Join investment campaigns</span>
+                </li>
+                <li className="flex items-center py-2 md:py-0">
+                  <CheckIcon className="size-6 text-green-300 mr-2"/>
+                  <span>Track total investments per cycle</span>
+                </li>
+                <li className="flex items-center py-2 md:py-0">
+                  <CheckIcon className="size-6 text-green-300 mr-2"/>
+                  <span>Ensure deposits meet required amounts.
+                  </span>
+                </li>
+                <li className="flex items-center py-2 md:py-0">
+                  <CheckIcon className="size-6 text-green-300 mr-2"/>
+                  <span>Withdraw funds after campaign completion</span>
+                </li>
+                <li className="flex items-center py-2 md:py-0">
+                  <CheckIcon className="size-6 text-green-300 mr-2"/>
+                  <span>Receive returns plus interest.</span>
+                </li>
+              </ul>
+              <Link 
+                href='' 
+                className="bg-emerald-500 shadow-md py-2 px-4 rounded-full text-center text-white font-bold text-xl hover:bg-blue-200 hover:text-cyan-950">
+                  Invest on  Project
+              </Link>
             </div>
           </div>
         </div>
+
         {/* <div
           onClick={() => {
             writeAsync();
@@ -55,9 +149,36 @@ const Home: NextPage = () => {
         >
           TEST TX
         </div> */}
-      </div>
+      </main>
     </>
   );
 };
 
 export default Home;
+
+const stats = [
+  {
+    percentage: '1000+',
+    description: 'active users',
+    borderColor: 'border-teal-600',
+    rotation: 'rotate-[324deg]',
+  },
+  {
+    percentage: '50+',
+    description: 'Current Cycle target',
+    borderColor: 'border-gray-400',
+    rotation: 'rotate-[252deg]',
+  },
+  {
+    percentage: '45+',
+    description: 'Current cycle invested',
+    borderColor: 'border-orange-500',
+    rotation: 'rotate-[270deg]',
+  },
+  {
+    percentage: '10+',
+    description: 'Total number of founded projects',
+    borderColor: 'border-blue-700',
+    rotation: 'rotate-[226.8deg]',
+  },
+];
